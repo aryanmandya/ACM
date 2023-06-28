a259b3ea840dd669b6076d965c4b22fe2c8e5d32
aryanmandya <101258430+aryanmandya@users.noreply.github.com>
2023-06-28 23:51:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
-
-const { User } = require('./models/shortUrl')
+const ShortUrl = require('./models/shortUrl.js')
 const app = express()
 
-mongoose.connect('mongodb+srv://aryanmandya4:n2GfAIip4fJCOIje@cluster0.f9eubjx.mongodb.net/', {
+mongoose.connect('mongodb://localhost/urlShortener', {
   useNewUrlParser: true, useUnifiedTopology: true
 })
 
@@ -12,18 +11,18 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', async (req, res) => {
-  const shortUrls = await User.find()
+  const shortUrls = await ShortUrl.find()
   res.render('index', { shortUrls: shortUrls })
 })
 
-app.post('/User', async (req, res) => {
-  await User.create({ full: req.body.fullUrl })
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl })
 
   res.redirect('/')
 })
 
-app.get('/:User', async (req, res) => {
-  const shortUrl = await User.findOne({ short: req.params.User })
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
   if (shortUrl == null) return res.sendStatus(404)
 
   shortUrl.clicks++

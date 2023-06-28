4b26bb5ea2ed4f0a1bf549c6eafa3fe214ffc609
aryanmandya <101258430+aryanmandya@users.noreply.github.com>
2023-06-28 17:36:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,18 +12,18 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', async (req, res) => {
-  const shortUrls = await ShortUrl.find()
+  const shortUrls = await User.find()
   res.render('index', { shortUrls: shortUrls })
 })
 
 app.post('/User', async (req, res) => {
-  await ShortUrl.create({ full: req.body.fullUrl })
+  await User.create({ full: req.body.fullUrl })
 
   res.redirect('/')
 })
 
 app.get('/:User', async (req, res) => {
-  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+  const shortUrl = await User.findOne({ short: req.params.shortUrl })
   if (shortUrl == null) return res.sendStatus(404)
 
   shortUrl.clicks++
